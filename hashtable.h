f53dfa44f93be07ed0eaa2cdc0adc8268f818ad8
Rafael Kallis <rafael@rafaelkallis.com>
2015-08-16 01:30:11 +0300

    Working

--- hashtable.h
+++ hashtable.h
@@ -18,8 +18,9 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
-#define hashtable_MULT_CONSTANT 0.6180339887498948482
 #define hashtable_MAX_LOAD_FACTOR 0.5
+
+#define fractional_part(whole) ((whole - (long)whole))
 
 #define hashtable_insufficient_memory_error()   \
     do{                                         \
@@ -27,6 +28,8 @@
      exit(EXIT_FAILURE);                        \
     }while(0)
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include "murmur3.h"
@@ -35,99 +38,111 @@
 
     typedef uint32_t hash_type;
 
+    typedef uint32_t key_type;
+
 /*                                  */
 
-enum hashtable_bucket_status{FREE, OCCUPIED, DELETED};
-
-struct hashtable_bucket{
-    enum hashtable_bucket_status status;
+struct hashtable_bucket_chaining{
     void * data;
+    struct hashtable_bucket_chaining * next;
 };
 
-struct hashtable{
-    hash_type seed,items,buckets;
-    struct hashtable_bucket * table;
-    void * (*get_key)(void* data);
+struct hashtable_chaining{
+    hash_type items,buckets;
+    struct hashtable_bucket_chaining ** table;
+    key_type * (*get_key)(void* data);
 };
 
-typedef struct hashtable hashtable;
+typedef struct hashtable_chaining hashtable;
 
-static int srand_check = 0;
-
-static inline hash_type hashtable_hash(_hashtable, data, probe_n)
-    struct hashtable * _hashtable;
-    void* data;
-    hash_type probe_n;
+static inline struct hashtable_bucket_chaining * hashtable_new_bucket_chaining(data,next)
+    void * data;
+    struct hashtable_bucket_chaining * next;
 {
-    hash_type hash;
-    MurmurHash3_x64_128(_hashtable->get_key(data), sizeof(hash_type), _hashtable->seed, &hash);
-    return _hashtable->buckets * fmod((hashtable_MULT_CONSTANT * hash)+probe_n, 1);
+    struct hashtable_bucket_chaining * new_bucket_chaining;
+    if ((new_bucket_chaining = (struct hashtable_bucket_chaining*)malloc(sizeof(struct hashtable_bucket_chaining))) == NULL)
+        hashtable_insufficient_memory_error();
+    new_bucket_chaining->data = data;
+    new_bucket_chaining->next = next;
+    return new_bucket_chaining;
 }
 
-struct hashtable * hashtable_Init(get_key, init_size)
-    void * (*get_key)(void*);
+/* Multiplicative hash using Knuth's famous 1/GoldenRatio Constant */
+/*          h(x) = m * fractional_part(x * 0.618) */
+static inline hash_type hashtable_hash(buckets, key)
+    hash_type buckets;
+    key_type * key;
+{
+    double fraction = 0.6180339887498948482 * *key;
+    return (hash_type)(buckets * (fraction - (long)fraction));
+}
+
+struct hashtable_chaining * hashtable_Init(get_key, init_size)
+    key_type * (*get_key)(void*);
     hash_type init_size;
 {
-    if(!srand_check) srand(time(0)), srand_check = rand();
-        
-    struct hashtable * new_hashtable;
+    struct hashtable_chaining * new_hashtable;
     
-    if((new_hashtable = (struct hashtable*) malloc(sizeof(struct hashtable))) == NULL)
+    if((new_hashtable = (struct hashtable_chaining*) malloc(sizeof(hashtable))) == NULL)
         hashtable_insufficient_memory_error();
         
 #warning maybe replaceable with rehash()
-    new_hashtable->seed         = rand();
     new_hashtable->items        = 0;
     new_hashtable->buckets      = init_size;
     new_hashtable->get_key      = get_key;
         
-    if ((new_hashtable->table   = (struct hashtable_bucket *)malloc(sizeof(struct hashtable_bucket) * init_size)) == NULL)
+    if ((new_hashtable->table   = (struct hashtable_bucket_chaining **)malloc(sizeof(struct hashtable_bucket_chaining) * init_size)) == NULL)
         hashtable_insufficient_memory_error();
         
     return new_hashtable;
 }
 
 void hashtable_Insert(_hashtable, data)
-    struct hashtable * _hashtable;
+    struct hashtable_chaining * _hashtable;
     void * data;
 {
 #warning check for loadfactor
 #warning dynamic resize maybe
-    register hash_type probe_n = 0, hash;
-    while(_hashtable->table[hash = hashtable_hash(_hashtable, data, probe_n)].status == OCCUPIED){ probe_n++; }
-    
-#warning maybe data doesnt get assigned
-    _hashtable->table[hash].data = data;
+    hash_type hash = hashtable_hash(_hashtable->buckets, _hashtable->get_key(data)); /*hashtable_hash_f(_hashtable->buckets, *(double*)data);*/
+    _hashtable->table[hash] = hashtable_new_bucket_chaining(data, _hashtable->table[hash]);
 }
 
 void * hashtable_Query(_hashtable, data, compar)
-    struct hashtable * _hashtable;
+    struct hashtable_chaining * _hashtable;
     void * data;
     int (*compar)(void*,void*);
 {
-    register hash_type probe_n = 0, hash;
+    hash_type hash = hashtable_hash(_hashtable->buckets, data);
+    struct hashtable_bucket_chaining * iterator = _hashtable->table[hash];
     
-    while(_hashtable->table[hash = hashtable_hash(_hashtable, data, probe_n)].status != FREE){
-        if(compar(_hashtable->table[hash].data, data) == 0 )
-            return _hashtable->table[hash].data;
-        else
-            probe_n++;
+    while(iterator){
+        if(compar(iterator->data, data) == 0 ) return iterator;
+        else iterator = iterator->next;
     }
+    
     return NULL;
 }
 
-void hashtable_Delete(_hashtable, data, compar)
-    struct hashtable * _hashtable;
+void hashtable_Delete(_hashtable, data, compar, destroy)
+    struct hashtable_chaining * _hashtable;
     void * data;
     int (*compar)(void*,void*);
+    void (*destroy)(void* data);
 {
-    register hash_type probe_n = 0, hash;
+    hash_type hash = hashtable_hash(_hashtable->buckets, data);
+    struct hashtable_bucket_chaining * temp = _hashtable->table[hash], * prev = NULL;
     
-    while(_hashtable->table[hash = hashtable_hash(_hashtable, data, probe_n)].status != FREE){
-        if(compar(_hashtable->table[hash].data, data) == 0 )
-            _hashtable->table[hash].status = DELETED;
-        else
-            probe_n++;
+    while(temp){
+        if(compar(temp->data,data)==0)break;
+        else prev = temp, temp = temp->next;
+    }
+    
+    if (temp){
+        if (prev) prev ->next = temp->next;
+        else _hashtable->table[hash] = temp->next;
+                
+        destroy(temp->data);
+        free(temp);
     }
 }
 
